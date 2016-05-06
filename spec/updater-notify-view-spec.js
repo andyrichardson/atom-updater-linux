e06de0c0d2dba2313b8f85e2fb8b1e591ce04b45
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2016-05-06 10:36:52 -0300

    start of project

---
+++ spec/updater-notify-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import UpdaterNotifyView from '../lib/updater-notify-view';
+
+describe('UpdaterNotifyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
