37beeb71d081f3ee44426bf4131efc34b7c709ef
Andy Richardson <andy.john.richardson@gmail.com>
2018-01-02 22:27:58 +0000

    Big clean up of structure and notificaitons

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use babel';
 
 import Settings from './lib/settings';
+import Package from './lib/package';
 
-export default PackageSettings = Settings;
+export default {
+  config: Settings.config,
+  activate: Package.activate
+}
