2d8e06a53d89c5b0b46a9507a9ff175846a3a5a0
Andy Richardson <arichardson@bravurasolutions.com>
2018-01-04 18:51:52 +0000

    Add linting

--- spec/atom-updater-linux-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import UpdaterNotifyView from '../lib/atom-updater-linux-view';
-
-describe('UpdaterNotifyView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
